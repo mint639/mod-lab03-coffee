3a0340195d19e6243fb79961eb56d844fb1a52aa
mint639 <101053166+mint639@users.noreply.github.com>
2022-05-29 20:24:25 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,13 @@
 #include "Automata.h"
 
 int main(int argc, char* argv[]) {
-	CoffeeMachine coffeemachine = CoffeeMachine();
-	coffeemachine.on();
-	coffeemachine.printMenu();
-	coffeemachine.coin(100);
-	coffeemachine.choice(Product::AMERICANO);
-	coffeemachine.cook();
-	coffeemachine.finish();
-	coffeemachine.off();
-	return 0;
+    CoffeeMachine coffeemachine = CoffeeMachine();
+    coffeemachine.on();
+    coffeemachine.printMenu();
+    coffeemachine.coin(100);
+    coffeemachine.choice(Product::AMERICANO);
+    coffeemachine.cook();
+    coffeemachine.finish();
+    coffeemachine.off();
+    return 0;
 }
